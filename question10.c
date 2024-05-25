5ef2b746963ffc60210ede94830c2e6db6066e1e
rakshabajpai121 <rakshabajpai075@gmail.com>
2024-05-25 22:13:26 +0530

    Update question10.c

--- question10.c
+++ question10.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <math.h>
 
+/* 
+10. Write a C program that calculates the volume of a sphere.
+(Formula : V = 4/3*π*r*r*r)
+Expected Output:
+Input the radius of the sphere : 2.56
+The volume of sphere is 70.276237.
+*/
+// Raksha bajpayi
+// 2303510100070
+int main() {
+    float radius, volume;
+    const float pi = 3.14159265359;
+
+    printf("Input the radius of the sphere : ");
+    scanf("%f", &radius);
+
+    volume = (4.0 / 3.0) * pi * pow(radius, 3);
+
+    printf("The volume of sphere is %f.\n", volume);
+
+    return 0;
+}
