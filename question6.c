780fdc6d8177c693184b8da322d6ec537ba72188
rakshabajpai121 <rakshabajpai075@gmail.com>
2024-05-25 22:08:14 +0530

    Update question6.c

--- question6.c
+++ question6.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+//2303510100070
+//Raksha bajpayi
+int main() {
+    float kmPerHour, milesPerHour;
+    printf("Input kilometres per hour: ");
+    scanf("%f", &kmPerHour);
+    milesPerHour = kmPerHour * 0.621371;
+    printf("%.6f miles per hour\n", milesPerHour);
+    return 0;
+}
