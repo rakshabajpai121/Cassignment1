06aa5c29acde67fd245d4424227013ff9d07bc09
rakshabajpai121 <rakshabajpai075@gmail.com>
2024-05-25 22:07:24 +0530

    Update question5.c

--- question5.c
+++ question5.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+//2303510100070
+//Raksha bajpayi
+int main() {
+    int num1, num2;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    printf("Sum: %d\n", num1 + num2);
+    return 0;
+}
